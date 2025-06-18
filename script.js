b238c4340713310b433dbd9172e36bb6033cfe3a
Jerome Daniel C. Rosales <131425913+Jaydee1430@users.noreply.github.com>
2025-06-18 11:22:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,95 @@
-// Ensure script is loaded and waits for HTML
+
 document.addEventListener("DOMContentLoaded", function () {
   const members = {
     jerome: {
         name: "Jerome Rosales",
-        contribution: 3000,
-        payout: "June 25, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       jaja: {
         name: "Jaja",
-        contribution: 3000,
-        payout: "July 2, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       lina: {
         name: "Lina",
-        contribution: 3000,
-        payout: "July 9, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       linda: {
         name: "Lina",
-        contribution: 3000,
-        payout: "July 16, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       dennis: {
         name: "Dennis",
-        contribution: 3000,
-        payout: "June 25, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       jane: {
         name: "Jane",
-        contribution: 3000,
-        payout: "July 2, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       christian: {
         name: "Christian",
-        contribution: 3000,
-        payout: "July 9, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       shai: {
         name: "Shai",
-        contribution: 3000,
-        payout: "July 16, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       apyang: {
         name: "Apyang",
-        contribution: 3000,
-        payout: "June 25, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       richardPan: {
         name: "Richard Pantoja",
-        contribution: 3000,
-        payout: "July 2, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       casie: {
         name: "Casie",
-        contribution: 3000,
-        payout: "July 9, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       raymond: {
         name: "Raymond",
-        contribution: 3000,
-        payout: "July 16, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       ryan: {
         name: "Ryan",
-        contribution: 3000,
-        payout: "June 25, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       pie: {
         name: "Pie",
-        contribution: 3000,
-        payout: "July 2, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       richardSan: {
         name: "Richard Santos",
-        contribution: 3000,
-        payout: "July 9, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       felma: {
         name: "Felma",
-        contribution: 3000,
-        payout: "July 16, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
       },
       cresing: {
         name: "Cresing",
-        contribution: 3000,
-        payout: "July 16, 2025"
+        contribution: 0,
+        payout: "Dec 14, 2025"
+      },
+      ynna: {
+        name: "Ynna",
+        contribution: 0,
+        payout: "Dec 14, 2025"
       }
   };
 
